ac5409abdd37e893a0d52b2d2b28a2b4bb01fb73
Rob J Goedman <goedman@mac.com>
2018-10-30 20:46:01 -0600

    Testing with new .travis.yml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,16 @@
-using StatisticalRethinking
+using StatisticalRethinking, Literate
 using Test
 
+ProjDir = joinpath(@__DIR__, "..", "chapters", "00")
+cd(ProjDir) do
+  
+println(ProjDir)
+
+Literate.markdown("snippets00.0.jl", ProjDir, name="snippets00.1", documenter=true)
+Literate.markdown("snippets00.0.jl", ProjDir, name="snippets00.2", documenter=false)
+Literate.script("snippets00.0.jl", ProjDir, name="snippets00.3")
+Literate.notebook("snippets00.0.jl", ProjDir, name="snippets00.4")
+
 @test 1 == 1
+
+end

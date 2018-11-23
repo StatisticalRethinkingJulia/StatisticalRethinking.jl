a8173cf8094e84593f2688f52f9ae01ea999ff8f
Rob J Goedman <goedman@mac.com>
2018-11-22 21:33:14 -0700

    Further map trials

---
+++ chapters/02/map/mohamed02.jl
@@ -0,0 +1,12 @@
+using Turing
+
+@model gdemo(x, y) = begin
+  #prior
+  theta ~ Uniform(0, 1)
+  #model
+  k ~ Binomial(9, theta)
+  return k
+end
+
+chn = sample(gdemo(), HMC(2000, 0.75, 5))
+describe(chn)

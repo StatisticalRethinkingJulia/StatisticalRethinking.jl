424fc071bde79433922739b060811cfdbcbf1740
Rob J Goedman <goedman@mac.com>
2018-11-25 08:53:59 -0700

    Added an initial doc stab

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,13 @@
 # StatisticalRethinking
 
-*Documentation goes here.*
+StatisticalRethinking is a Julia implementation of the code accompanying the book [StatisticalRethinking](https://xcelab.net/rm/statistical-rethinking/).
+
+
+```@meta
+CurrentModule = HyperDualNumbers
+```
+
+## maximum_a_posteriori
+```@docs
+maximum_a_posteriori(model, lower_bound, upper_bound)
+````
--- src/maximum_a_posteriori.jl
+++ src/maximum_a_posteriori.jl
@@ -1,3 +1,29 @@
+"""
+
+# maximum_a_posterior 
+
+Compute the maximum_a_posteriori of a model. 
+
+### Method
+```julia
+maximum_a_posteriori(model, lower_bound, upper_bound)
+```
+### Required arguments
+```julia
+* `model::Turing model`
+* `lower_bound::Float64`
+```
+
+### Return values
+```julia
+* `result`                       : Maximum_a_posterior vector
+```
+
+### Examples
+
+See ...
+
+"""
 function maximum_a_posteriori(model, lower_bound, upper_bound)
   
   vi = Turing.VarInfo()

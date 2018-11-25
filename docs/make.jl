f745927773135e87b90416c915fa346714784573
Rob J Goedman <goedman@mac.com>
2018-11-25 15:17:01 -0700

    Removed Literate calls from make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -12,40 +12,15 @@ const src_path = @__DIR__
 "Relative path using the StatisticalRethinking src/ directory."
 rel_path(parts...) = normpath(joinpath(src_path, parts...))
 
-CHAPTERS_ROOT = rel_path("../chapters") # Source directory
-
-# Target directories for Literate
-DOC_ROOT = rel_path("../docs/md")
-SN_ROOT = rel_path("../snippets")
-NB_ROOT = rel_path("../notebooks")
+DOC_ROOT = rel_path("../docs")
 
 chapters = ["00", "02", "03", "04", "05"]
-
-for chapter in chapters
-  ProjDir = joinpath(@__DIR__, "..", "chapters", chapter)
-  println("\nSwitching to directory: $ProjDir\n")
-  !isdir(ProjDir) && break
-  cd(ProjDir) do
-  
-    mdname = "snippets" * chapter
-
-    Literate.markdown(fname*".jl", joinpath(DOC_ROOT, chapter), name=fname, documenter=true)
-    Literate.script(fname*".jl", joinpath(SN_ROOT, chapter), name=fname)
-    Literate.notebook(fname*".jl", joinpath(NB_ROOT, chapter), name=fname)
-   # println()
-    
-    if isfile(joinpath(SN_ROOT, chapter, fname, ".jl")      
-      include(joinpath(SN_ROOT, chapter, fname, ".jl"))      
-    end
-  end
-end
-
 
 makedocs(root = DOC_ROOT,
     modules = Module[],
     sitename = "StatisticalRethinking.jl",
     pages = vcat(Any["index.md"],
-      Any["snippet_$(chapter).md" for chapter in chapter])
+      Any["md/snippet_$(chapter).md" for chapter in chapter])
 )
 
 deploydocs(root = DOC_ROOT,

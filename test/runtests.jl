14e334ca3f4e9d33a2ff93a1e1c0c60ff15ecd72
Rob J Goedman <goedman@mac.com>
2018-11-26 17:03:03 -0700

    Chapters 0 and 2 initial versions done

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,22 @@
 using StatisticalRethinking, Literate
 using Test
 
-#chapters = ["00", "02", "03", "04"]
-chapters = ["00", "02"]
+chapters = ["00", "02", "03", "04"]
 DocDir = joinpath(@__DIR__, "..", "docs", "src")
 
 for chapter in chapters
   ProjDir = joinpath(@__DIR__, "..", "chapters", chapter)
   SnippetDir = joinpath(@__DIR__, "..", "snippets", chapter)
+  !isdir(SnippetDir) && mkdir(SnippetDir)
   NotebookDir = joinpath(@__DIR__, "..", "notebooks", chapter)
+  !isdir(NotebookDir) && mkdir(NotebookDir)
   println("\nSwitching to directory: $ProjDir\n")
   !isdir(ProjDir) && break
   cd(ProjDir) do
     
     filelist = readdir()
     for file in filelist
-      if file[1:4] == "clip" && file[end-2:end] == ".jl"  
+      if !isdir(file) && file[1:4] == "clip" && file[end-2:end] == ".jl"  
         
         fname = "snippets" * file[5:end-3]
         
@@ -30,4 +31,5 @@ for chapter in chapters
       end # if file[1:4]
     end # for file
   end # cd
+  println()
 end # for chapter

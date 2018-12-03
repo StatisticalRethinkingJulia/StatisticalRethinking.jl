e42090d946f651db35a3a2faa9089d2ce4bdbd58
Richard Torkar <richard.torkar@gmail.com>
2018-12-03 09:08:18 +0100

    - A bunch of new models added Ch8 and Ch10. - Some cleanup of the first models in Ch8.

--- chapters/08/m8.1.jl
+++ chapters/08/m8.1.jl
@@ -1,8 +1,7 @@
-# m8.1stan 
-# richard.torkar@gmail.com
+# m8.1stan
 #
-# m8.1stan is the first model in the Statistical Rethinking book (pp. 249) using Stan. 
-# Here we will use Turing's NUTS support, which is currently (2018) the original NUTS by 
+# m8.1stan is the first model in the Statistical Rethinking book (pp. 249) using Stan.
+# Here we will use Turing's NUTS support, which is currently (2018) the original NUTS by
 # Hoffman & Gelman, http://www.stat.columbia.edu/~gelman/research/published/nuts.pdf
 # and not the one that's in Stan 2.18.2, i.e., Appendix A.5 in:
 # https://arxiv.org/abs/1701.02434
@@ -10,7 +9,7 @@
 # The StatisticalRethinking pkg uses, e.g., Turing, CSV, DataFrames
 using StatisticalRethinking
 
-d = CSV.read(joinpath(dirname(Base.pathof(StatisticalRethinking)), "..", "data", 
+d = CSV.read(joinpath(dirname(Base.pathof(StatisticalRethinking)), "..", "data",
     "rugged.csv"), delim=';')
 size(d) # Should be 234x51
 
@@ -19,7 +18,7 @@ d = hcat(d, map(log, d[Symbol("rgdppc_2000")]))
 rename!(d, :x1 => :log_gdp) # Rename our col x1 => log_gdp
 
 # Now we need to drop every row where rgdppc_2000 == missing
-# When this (https://github.com/JuliaData/DataFrames.jl/pull/1546) hits DataFrame it'll be 
+# When this (https://github.com/JuliaData/DataFrames.jl/pull/1546) hits DataFrame it'll be
 # conceptually easier:
 # i.e., completecases!(d, :rgdppc_2000)
 notisnan(e) = !ismissing(e)
@@ -27,22 +26,20 @@ dd = d[map(notisnan, d[:rgdppc_2000]), :]
 size(dd) # should equal 170 x 52
 
 @model m8_1stan(y, x₁, x₂) = begin
-    N = length(y)
     σ ~ Truncated(Cauchy(0, 2), 0, Inf)
     βR ~ Normal(0, 10)
     βA ~ Normal(0, 10)
     βAR ~ Normal(0, 10)
     α ~ Normal(0, 100)
 
-    for i ∈ 1:N
+    for i ∈ 1:length(y)
         y[i] ~ Normal(α + βR * x₁[i] + βA * x₂[i] + βAR * x₁[i] * x₂[i], σ)
     end
-    #return βR, βA, σ
 end
 
 # Test to see that the model is sane. Use 2000 for now, as in the book.
 # Need to set the same stepsize and adapt_delta as in Stan...
-posterior = sample(m8_1stan(dd[:,:log_gdp], dd[:,:rugged], dd[:,:cont_africa]), 
+posterior = sample(m8_1stan(dd[:,:log_gdp], dd[:,:rugged], dd[:,:cont_africa]),
     Turing.NUTS(2000, 1000, 0.95))
 describe(posterior)
 # Output reg. params of interest:
@@ -60,4 +57,3 @@ describe(posterior)
 # bA    -1.97   0.23      -2.31      -1.58   351    1
 # bAR    0.40   0.13       0.20       0.63   350    1
 # sigma  0.95   0.05       0.86       1.03   566    1
-
---
+++ chapters/10/m10.3.jl
@@ -0,0 +1,33 @@
+using StatisticalRethinking
+using StatsFuns #logistic() function
+
+d = CSV.read(joinpath(dirname(Base.pathof(StatisticalRethinking)), "..", "data",
+    "chimpanzees.csv"), delim=';')
+size(d) # Should be 504x8
+
+# pulled_left, condition, prosoc_left
+@model m10_3(y, x₁, x₂) = begin
+    α ~ Normal(0, 10)
+    βp ~ Normal(0, 10)
+    βpC ~ Normal(0, 10)
+
+    for i ∈ 1:length(y)
+        p = α + (βp + βpC * x₁[i]) * x₂[i]
+        y[i] ~ Binomial(1, logistic(p))
+    end
+end
+
+posterior = sample(m10_3(d[:,:pulled_left], d[:,:condition], d[:,:prosoc_left]),
+    Turing.NUTS(10000, 1000, 0.95))
+describe(posterior)
+#     Empirical Posterior Estimates:
+#              Mean           SD        Naive SE       MCSE         ESS
+# α           0.053228176  0.148432403 0.0033190494 0.0072162528  423.091170
+# βpC        -0.074156932  0.278219321 0.0062211731 0.0279932431   98.779800
+# βp          0.604297351  0.241527734 0.0054007243 0.0212696753  128.947312
+
+#StatisticalRethinking
+#      Mean StdDev lower 0.89 upper 0.89 n_eff Rhat
+# a    0.05   0.13      -0.15       0.25  3284    1
+# bp   0.62   0.22       0.28       0.98  3032    1
+# bpC -0.11   0.26      -0.53       0.29  3184    1

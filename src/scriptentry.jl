4e1b088082dd7d3088a0054783ae668f1e6778bc
Rob J Goedman <goedman@mac.com>
2019-01-13 09:59:06 -0700

    Added m12.6.2.jl

--- src/scriptentry.jl
+++ src/scriptentry.jl
@@ -127,6 +127,7 @@ script_dict = DataStructures.OrderedDict{AbstractString, Vector{ScriptEntry}}(
     scriptentry("m12.4.jl", exe = false, doc = false),
     scriptentry("m12.5.jl", exe = false, doc = false),
     scriptentry("m12.6.jl", exe = false, doc = false),
-    scriptentry("m12.6s.jl")
+    scriptentry("m12.6.1s.jl"),
+    scriptentry("m12.6.2s.jl")
   ]
 );

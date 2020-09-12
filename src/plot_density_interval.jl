00ea8684f9babc2119e86d2fd64748a32a5abf2c
Rob J Goedman <goedman@mac.com>
2020-09-12 09:21:36 -0600

    Rel 3.0.0

--- src/plot_density_interval.jl
+++ src/plot_density_interval.jl
@@ -16,9 +16,8 @@ $(SIGNATURES)
 ```julia
 * `color = [:grey, :blue]              : Denity and fill colors
 * `alpha = 0.25`                       : Transparency factor
-* `x::AbstractString`                  : x axis label
-* `y::AbstractString`                  : y axis label
-* `p = plot()`                         : Plot used as starting point
+* `xlab::AbstractString`               : x axis label
+* `ylab::AbstractString`               : y axis label
 ```
 ### Return values
 ```julia
@@ -26,20 +25,55 @@ $(SIGNATURES)
 ```
 
 """
-function plot_density_interval(s::Vector{Float64},
-  bounds::Vector{Float64};
-  color = [:grey, :blue],
-  alpha = 0.25,
-  x = :x,
-  y = "density",
-  p = plot())
+function plot_density_interval(s::Vector{Float64}, bounds::Vector{Float64};
+  colors = [:grey, :blue], alpha = 0.25, xlab = :x, ylab = "Density", kwargs...)
   k = kde(s)
-  p = plot!(k.x, k.density, xlim=(0, 1), 
-    xlab=x, ylab="density", color=color[1])
+  p = plot(k.x, k.density;
+    xlim=(0, 1), xlab=xlab, ylab=ylab, color=colors[1], kwargs...)
   indxs = filter(i -> bounds[1] < k.x[i] < bounds[2], 1:length(k.x))
   for i in indxs
-    plot!(p, [k.x[i], k.x[i]], [0.0, k.density[i]], alpha=alpha,
-      color=color[2], leg=false)
+    plot!(p, [k.x[i], k.x[i]], [0.0, k.density[i]], alpha=alpha;
+      color=colors[2], leg=false, kwargs...)
+  end
+  p
+end
+
+"""
+
+# plot_density_interval!
+
+$(SIGNATURES)
+
+# Extended help
+
+### Required arguments
+```julia
+* `s::Vector{Float64`                  : Samples
+* `bounds::Vector{Float64}`            : Lower and upper bound for interval
+```
+
+### Keyword arguments
+```julia
+* `color = [:grey, :blue]              : Denity and fill colors
+* `alpha = 0.25`                       : Transparency factor
+* `xlab::AbstractString`               : x axis label
+* `ylab::AbstractString`               : y axis label
+```
+### Return values
+```julia
+* `p`                                  : Resulting plot
+```
+
+"""
+function plot_density_interval!(s::Vector{Float64}, bounds::Vector{Float64};
+  colors = [:grey, :blue], alpha = 0.25, xlab = :x, ylab = "Density", kwargs...)
+  k = kde(s)
+  p = plot!(k.x, k.density;
+    xlim=(0, 1), xlab=xlab, ylab=ylab, color=colors[1], kwargs...)
+  indxs = filter(i -> bounds[1] < k.x[i] < bounds[2], 1:length(k.x))
+  for i in indxs
+    plot!(p, [k.x[i], k.x[i]], [0.0, k.density[i]], alpha=alpha;
+      color=colors[2], leg=false, kwargs...)
   end
   p
 end

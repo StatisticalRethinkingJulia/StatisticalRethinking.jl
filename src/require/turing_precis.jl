90c8576295b2b2544dcba9c91fd91f6286d97788
Rob J Goedman <goedman@mac.com>
2020-10-05 20:21:28 -0600

    Rel 3.0.0

--- src/require/turing_precis.jl
+++ src/require/turing_precis.jl
@@ -8,7 +8,7 @@ $(SIGNATURES)
 function precis(nt::NamedTuple; io = stdout, digits = 2, depth = Inf, alpha = 0.11)
   post = rand(nt.distr, 10_000)
   df = DataFrame(post', [keys(nt.coef)...])
-  precis(df)
+  Text(precis(df; io=String))
 end
 
 """
@@ -21,5 +21,5 @@ $(SIGNATURES)
 function precis(m::DynamicPPL.Model; io = stdout, digits = 2, depth = Inf, alpha = 0.11)
     chns = mapreduce(c -> sample(m, NUTS(0.65), 2000), chainscat, 1:4)
     df = DataFrame(Array(chns), names(chns, [:parameters]))
-    precis(df)
+    Text(precis(df; io=String))
 end

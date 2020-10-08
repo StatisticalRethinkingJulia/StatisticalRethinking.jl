69041dac93062f672e481afbc46deed31ce26aa1
Rob J Goedman <goedman@mac.com>
2020-10-08 12:16:48 -0600

    Rel 3.0.0

--- src/require/turing_plotcoef.jl
+++ src/require/turing_plotcoef.jl
@@ -56,21 +56,21 @@ function plotcoef(
         df = DataFrame(post', [keys(quap_mdl.coef)...])
         m, l, u = estimparam(df)
         d = Dict{Symbol, NamedTuple}()
-        for (indx, par) in enumerate(names(chns, [:parameters]))
+        for (indx, par) in enumerate([keys(quap_mdl.coef)...])
             d[par] = (mean=m[indx], lower=l[indx], upper=u[indx])
         end
         s[mindx] =   (; d...)
     end
   end
+
   xmin = 0; xmax = 0.0
   for i in 1:length(s)
     for par in pars
       syms = Symbol.(keys(s[i]))
       if Symbol(par) in syms
-        mp = mean(s[i][Symbol(par)])
-        sp = std(s[i][Symbol(par)])
-        xmin = min(xmin, mp - sp)
-        xmax = max(xmax, mp + sp)
+        mp = s[i][par].mean
+        xmin = min(xmin, s[i][par].lower)
+        xmax = max(xmax, s[i][par].upper)
       end
     end
   end
@@ -102,9 +102,10 @@ function plotcoef(
       syms = Symbol.(keys(s[mindx]))
       if Symbol(par) in syms
         ypos = (line - 1)
-        mp = mean(s[mindx][Symbol(par)])
-        sp = std(s[mindx][Symbol(par)])
-        plot!([mp-sp, mp+sp], [ypos, ypos], leg=false, color=colors[pindx])
+        mp = s[mindx][Symbol(par)].mean
+        lower = s[mindx][Symbol(par)].lower
+        upper = s[mindx][Symbol(par)].upper
+        plot!([lower, upper], [ypos, ypos], leg=false, color=colors[pindx])
         scatter!([mp], [ypos], color=colors[pindx])
       end
     end
@@ -151,68 +152,10 @@ function plotcoef(
   fig="", title="", func=nothing,
   sampler=NUTS(0.65), nsamples=2000, nchains=4)
 
-  # mname = nameof(mdl)
-  levels = length(pars)
-  colors = [:blue, :red, :green, :darkred, :black]
+  plotcoef([mdl], [mname], pars;
+    fig, title, func, sampler, nsamples, nchains)
 
-  if isnothing(func)
-      chns = mapreduce(c -> sample(mdl, sampler, nsamples),
-          chainscat, 1:nchains)
-      df = DataFrame(Array(chns), names(chns, [:parameters]))
-      m, l, u = estimparam(df)
-      d = Dict{Symbol, NamedTuple}()
-      for (indx, par) in enumerate(names(chns, [:parameters]))
-          d[par] = (mean=m[indx], lower=l[indx], upper=u[indx])
-      end
-      s[mindx] =   (; d...)
-  else
-      quap_mdl = quap(mdl)
-      post = rand(quap_mdl.distr, 10_000)
-      df = DataFrame(post', [keys(quap_mdl.coef)...])
-      m, l, u = estimparam(df)
-      d = Dict{Symbol, NamedTuple}()
-      for (indx, par) in enumerate(names(chns, [:parameters]))
-          d[par] = (mean=m[indx], lower=l[indx], upper=u[indx])
-      end
-      s[mindx] =   (; d...)
-  end
-
-  xmin = 0; xmax = 0.0
-  for par in pars
-    syms = Symbol.(keys(s))
-    if Symbol(par) in syms
-      mp = mean(s[Symbol(par)])
-      sp = std(s[Symbol(par)])
-      xmin = min(xmin, mp - sp)
-      xmax = max(xmax, mp + sp)
-    end
-  end
-
-  ys = String[""]
-  for i in 1:length(pars)
-   append!(ys, [String(pars[i])])
-  end
-
-  p = plot(xlims=(xmin, xmax), leg=false, framestyle=:grid)
-  length(title) > 0 && title!(title)
-  yran = range(0, stop=length(ys)-1, length=length(ys))
-  yticks!(yran, ys)
-
-  for (pindx, par) in enumerate(pars)
-    syms = Symbol.(keys(s))
-    if Symbol(par) in syms
-      lineno = pindx
-      mp = mean(s[Symbol(par)])
-      sp = std(s[Symbol(par)])
-      plot!([mp-sp, mp+sp], [lineno, lineno], leg=false, color=colors[pindx])
-      scatter!([mp], [lineno], color=colors[pindx])
-    end
-  end
-  if length(fig) > 0
-    savefig(p, fig)
-  end
-  (s, p)
-end
+ end
 
 export
   plotcoef

1a1ac90d5ea64e693c53713cc82b2961d8ac4eef
Rob J Goedman <goedman@mac.com>
2020-12-09 11:06:03 -0700

    Rel 3.1.5 - Fix quap() for single variable quap model.

--- src/quap.jl
+++ src/quap.jl
@@ -83,7 +83,11 @@ function sample(qm::NamedTuple; nsamples=4000)
   df = DataFrame()
   p = Particles(nsamples, qm.distr)
   for (indx, coef) in enumerate(qm.params)
-    df[!, coef] = p[indx].particles
+    if length(qm.params) == 1
+      df[!, coef] = p.particles
+    else
+      df[!, coef] = p[indx].particles
+    end
   end
   df
 end
@@ -92,5 +96,5 @@ end
 export
   mode_estimates,
   quap,
-  samples
+  sample
 	

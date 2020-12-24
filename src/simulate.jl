0ac437367364bb3b9a2486d67a44b1f61c9dcf6f
Rik Huijzer <t.h.huijzer@rug.nl>
2020-12-24 09:15:53 +0100

    Use rand(...) instead of rand(..., 1)[1]

--- src/simulate.jl
+++ src/simulate.jl
@@ -23,7 +23,7 @@ function simulate(df, coefs, var_seq)
   for j in 1:size(df, 1)
     for i in 1:length(var_seq)
       d = Normal(df[j, coefs[1]] + df[j, coefs[2]] * var_seq[i], df[j, coefs[3]])
-      m_sim[j, i] = rand(d, 1)[1]
+      m_sim[j, i] = rand(d)
     end
   end
   m_sim
@@ -57,7 +57,7 @@ function simulate(df, coefs, var_seq, coefs_ext)
     for i in 1:length(var_seq)
       d = Normal(df[j, coefs[1]] + df[j, coefs[2]] * var_seq[i] +
         df[j, coefs_ext[1]] * m_sim[j, i], df[j, coefs_ext[2]])
-      d_sim[j, i] = rand(d, 1)[1]
+      d_sim[j, i] = rand(d)
     end
   end
   (m_sim, d_sim)

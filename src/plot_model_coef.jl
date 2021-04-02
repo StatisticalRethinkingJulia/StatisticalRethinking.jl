541f20b3d80196b3420ca81287db2026b76fb3d1
Rob J Goedman <goedman@icloud.com>
2021-04-02 10:01:47 -0600

    Rel 3.4.1 - Added plot_model_coef(df, ...)

--- src/plot_model_coef.jl
+++ src/plot_model_coef.jl
@@ -57,5 +57,25 @@ function plot_model_coef(s::Vector{NamedTuple},  pars::Vector{Symbol};
   (s, p)
 end
 
+function plot_model_coef(df::DataFrame, pars::Vector{Symbol};
+    mname="", fig="", title="")
+
+  if length(mname) > 9
+    mname = mname[1:9]
+  end
+ 
+  s = Vector{NamedTuple}(undef, 1)
+  for mindx in 1:1
+    m, l, u = estimparam(df)
+    d = Dict{Symbol, NamedTuple}()
+    for (indx, par) in enumerate(names(df))
+      d[Symbol(par)] = (mean=m[indx], lower=l[indx], upper=u[indx])
+    end
+    s[mindx] =   (; d...)
+  end
+
+  plot_model_coef(s, pars; mnames=[mname], fig, title)
+end
+
 export
   plot_model_coef

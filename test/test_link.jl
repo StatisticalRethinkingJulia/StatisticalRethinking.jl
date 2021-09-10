1ab06685f9400876a0660da2e5b52520acd2845b
Max Lapan <max.lapan@gmail.com>
2021-09-10 10:00:53 +0200

    Link docs + other docs cleanups

--- test/test_link.jl
+++ test/test_link.jl
@@ -1,4 +1,5 @@
 d = DataFrame(:a => [1,2,3], :b => [2,3,4])
-x = 1:2
 ref = [[3,5,7], [5,8,11]]
-@test link(d, [:a, :b], x) == ref
+@test link(d, [:a, :b], 1:2) == ref
+
+@test link(d, (r,x) -> r.a + r.b * x, 1:2) == ref

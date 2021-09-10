4b68dbdb687bb9b7e5d09516f740dc32dfd4ad89
Max Lapan <max.lapan@gmail.com>
2021-09-10 19:51:26 +0200

    Fix PI signature

--- src/srtools.jl
+++ src/srtools.jl
@@ -87,8 +87,29 @@ Compute percentile central interval of data. Returns vector of bounds.
 
 $(SIGNATURES)
 
+## Required arguments
+* `data`: iterable over data values
+
+## Optional arguments
+* `prob::Float64=0.89`: percentile interval to calculate
+
+## Examples
+```jldoctest
+julia> using StatisticalRethinking
+
+julia> PI(1:10)
+2-element Vector{Float64}:
+ 1.495
+ 9.505
+
+julia> PI(1:10, prob=0.1)
+2-element Vector{Float64}:
+ 5.05
+ 5.95
+
+```
 """
-function PI(data, prob=0.89)
+function PI(data; prob::Float64=0.89)
     d = (1-prob)/2
     quantile(data, [d, 1-d])
 end
